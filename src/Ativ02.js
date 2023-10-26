3c5349c1fcae514b21e8285ab8dd566451e405f1
Drey Soares <112641522+Drey021@users.noreply.github.com>
2023-10-26 16:09:14 -0300

    Deletação do diretorio SRC para colocar outro arrumado em pastas

--- src/Ativ02.js
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-export default function Ativ02(){
-    return(
-        <>
-        <h2>Ativ02</h2>
-        <Link to="/">Voltar</Link>
-        </>
-    );
-}
--- src/Ativ05.js
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-export default function Ativ05(){
-    return(
-        <>
-        <h2>Ativ05</h2>
-        <Link to="/">Voltar</Link>
-        </>
-    );
-}

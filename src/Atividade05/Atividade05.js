d06fd472cca68e36542b649e63687226ea075f13
Drey Soares <112641522+Drey021@users.noreply.github.com>
2023-11-21 20:01:28 -0300

    Update Atividade05.js

--- src/Atividade05/Atividade05.js
+++ src/Atividade05/Atividade05.js
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './Atividade05.css';
+import Calculadora from './Calculadora';
 
 function Atividade05() {
   return (
     <>
-      <h1 className="title">Atividade 5</h1>
-
-      <span> <Link to={"/"}>Voltar</Link></span>
+      <h1 className="title">Atividade 4</h1>
+      <Calculadora />
+      <span><Link to={"/"}>⇐ VOLTAR</Link></span>
     </>
   );
 }
